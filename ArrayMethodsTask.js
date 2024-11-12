9517f62e9d8be311854e9b8af225a8eb884512f6
Chigbo <ezeokeke.chigbo@gmail.com>
2024-11-12 15:20:16 +0100

    updated task

--- ArrayMethodsTask.js
+++ ArrayMethodsTask.js
@@ -12,4 +12,61 @@ const squareNumbers = function(numbers){
     let result = numbers.map((number)=>number * number);
     return result;
 }
-module.exports = {numbersGreaterThan70, increaseStudentScoreBy5, squareNumbers};
+
+const distributedBooks = ()=>{
+    const newArray = [];
+    let answer = ["tales by moonlight", "tales by sunlight", "tales by greenlight", "tales by redlight"];
+    for (const members of answer){
+         newArray.push(members);
+    }
+    return newArray;
+}
+
+function identifyClasses(classTimings){
+    const array = [];
+    for(let elements of classTimings){
+        let splittedTime = elements.slice(5);
+        if(splittedTime === "PM"){
+            array.push(elements)
+    }
+ //classTimings.filter((timings)=>timings > );
+
+}
+return array;
+
+}
+
+const totalExpenses = (expenses)=>{
+    let sum = 0;
+    for(elements in expenses){
+        sum+=expenses[elements]
+    }
+return sum;
+
+}
+
+const returnStudentGrades = (array)=>{
+    let grades = [];
+    array.map((scores)=>{
+        if(scores >= 90 && scores <= 100){
+            grades.push("A");
+        }
+        if(scores >= 80 && scores <= 89){
+            grades.push("B");
+        }
+        if(scores >= 70 && scores <= 79){
+            grades.push("C");
+        }
+        if(scores >= 60 && scores <= 69){
+            grades.push("D");
+        }
+        if(scores <= 59){
+            grades.push("F");
+        }
+
+    })
+    return grades;
+
+}
+
+module.exports = {returnStudentGrades,totalExpenses, numbersGreaterThan70, increaseStudentScoreBy5, squareNumbers, distributedBooks, identifyClasses};
--- ArrayMethodsTask.test.js
+++ ArrayMethodsTask.test.js
@@ -1,4 +1,4 @@
-const{numbersGreaterThan70, increaseStudentScoreBy5, squareNumbers} = require("./ArrayMethodsTask");
+const{returnStudentGrades,numbersGreaterThan70, increaseStudentScoreBy5, squareNumbers, distributedBooks, identifyClasses, totalExpenses} = require("./ArrayMethodsTask");
 
 test("scores greater than or equal to 70", ()=>{
 let testScores = [20,80,50,70,40,30,90,75,35,95];
@@ -19,4 +19,36 @@ let numbers = [2,4,6,8,10];
 let result = squareNumbers(numbers);
 let answer = [4,16,36,64,100];
 expect(result).toEqual(answer);
+})
+
+test("distribute a different book to each member", ()=>{
+let members = ["Emily", "Jack", "Sophia", "Daniel"];
+let result = distributedBooks(members);
+let answer = ["tales by moonlight", "tales by sunlight", "tales by greenlight", "tales by redlight"];
+expect(result).toEqual(answer);
+})
+
+test("identify the classes offered in the afternoon", ()=>{
+let classTimings = ["9:00 AM", "11:00 AM", "1:00 PM","3:00 PM", "5:00 PM"];
+let result = identifyClasses(classTimings);
+let answer = ["1:00 PM", "3:00 PM","5:00 PM"];
+expect(result).toEqual(answer);
+})
+
+test("total amount spent on expenses",()=>{
+    const expenses = {"groceries": 150,
+                "dining out":100,
+                "transportation":50,
+                "entertainment": 80
+    };
+    let result = totalExpenses(expenses);
+    let answer = 380;
+    expect(result).toBe(answer);
+})
+
+test("return studen grades", ()=>{
+    const studentScores = [95, 78, 85, 60, 45, 92];
+    let result = returnStudentGrades(studentScores);
+    let answer = ["A","C","B","D","F","A"];
+    expect(result).toEqual(answer);
 })
