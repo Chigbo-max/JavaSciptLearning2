d92b29570038ad633b82ece927bc793cfb095fd2
Chigbo Ezeokeke <ezeokeke.chigbo@gmail.com>
2025-01-19 12:48:13 +0100

    updated calculator app in html/css/js

--- htmlcssproj/CalculatorAppHtmlJs/app.js
+++ htmlcssproj/CalculatorAppHtmlJs/app.js
@@ -1,29 +1,49 @@
 const inputValue = document.getElementById("inputValue");
 const buttons = document.querySelectorAll("button");
+let currentValue = "";
 
-buttons.forEach((button)=>{
-    button.addEventListener('click', (event)=>{
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
         const value = event.target.value;
-        console.log(value);
-
-        if(value === 'CE'){
+        if (value === 'CE') {
             inputValue.value = '';
+            currentValue = '';
         }
-        else if(value === "="){
-            try{
-        const result = math.evaluate(inputValue.value);
-        inputValue.value =  result;
+        else if (value === "=") {
+            try {
+                const result = math.evaluate(inputValue.value);
+                inputValue.value = result;
+                currentValue = result.toString();
+            }
+            catch (error) {
+                inputValue.value = 'Error'
+                currentValue = '';
+            }
         }
-        catch(error){
-            inputValue.value = 'Error'
+
+        else {
+            checkInput(value)
         }
-    }
-    
-    else{
-        inputValue.value += value;
-    }
-})
+    })
 
 
 })
 
+const checkInput = (value)=> {
+    if (currentValue === "" && value === "0") {
+        return;
+    }
+
+    if (currentValue === "0" && value === "0") {
+        currentValue = value;
+    }
+    else {
+        currentValue += value;
+    }
+    inputValue.value = currentValue;
+}
+
+//save the result
+//remove a leading zero if a non zero digit is entered
+//append the value to current input
+//update the input field
